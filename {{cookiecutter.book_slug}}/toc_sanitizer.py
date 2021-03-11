888a35b65c2101359d89e129fde8fd8f6dc291f0
Alejandro Fontal <jandrofontal@gmail.com>
2021-03-11 22:26:01 +0100

    fix: link should be fixed at every step, not only at the beginning

--- {{cookiecutter.book_slug}}/toc_sanitizer.py
+++ {{cookiecutter.book_slug}}/toc_sanitizer.py
@@ -8,7 +8,7 @@ for file in glob('{{ cookiecutter.book_slug }}/_build/html/*.html'):
         for line in fr.readlines():
             if line.startswith('<div class="toc">'):
                 old_values = line.split('#')
-                new_line = old_values[0] + '#' + '#'.join([c[0].lower() + c[1:] for c in old_values[1:]])
+                new_line = old_values[0] + '#' + '#'.join(['-'.join(l.lower() for l in c.split('-')) for c in old_values[1:]])
                 new_content += new_line
             else:
                 new_content += line
